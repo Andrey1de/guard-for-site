7f9eb4b0fd5b43dfde17d974b96b6eaaf49b7f36
Andrey1de <andrey1de@gmail.com>
2022-01-26 08:09:38 +0200

    first-gantt

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'guard-for-site'`, () => {
+  it(`should have as title 'gantt-chart'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('guard-for-site');
+    expect(app.title).toEqual('gantt-chart');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('guard-for-site app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('gantt-chart app is running!');
   });
 });
